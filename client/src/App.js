54c0c4169a33eef55ac843ceb537592b0562efc0
Yvette <yvettetran@gmail.com>
2018-11-20 14:45:43 -0800

    added react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as  Router, Route } from 'react-router-dom'
-import { Botton } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 // import { Link } from 'react-router'
 import Form from './components/Form';
 import LandingPage from './components/LandingPage'
@@ -11,14 +10,13 @@ import Login from './components/Login'
 class App extends Component {
   render() {
     return (
-      <div>
         <Router> 
-          <Route exact path='/' component={LandingPage} />
-          <Route exact path='/login' component={Login} />
-          {/* <Route exact path='/signUp' component={Signup} /> */}
+          <div>
+            <Route exact path='/' component={LandingPage} />
+            <Route exact path='/login' component={Login} />
+            {/* <Route exact path='/signUp' component={Signup} /> */}
+          </div>
         </Router>
-        <Form />
-      </div>
     )
   }
 }

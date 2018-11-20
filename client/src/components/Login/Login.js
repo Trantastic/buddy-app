e4d274f3bb6ba60cb9181376513d9e879a11e23c
Yvette <yvettetran@gmail.com>
2018-11-20 14:01:39 -0800

    created template for login page

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,30 @@
+import React, { Component, Link } from 'react'
+import { Button, Form, FormGroup } from 'react-bootstrap'
+
+class Login extends Component {
+
+    state = {
+        login: '',
+        password: '',
+    }
+
+    render() {
+        return(
+            <div>
+                <Form>
+                    <FormGroup>
+                        <Label for="userName">Username:</Label>
+                        <Input name="user" id="userName" placeholder="Enter Username" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password">Password:</Label>
+                        <Input type="password" name="password" id="password" placeholder="Enter Password" />
+                    </FormGroup>
+                    <Button color="success"><Link to='/quiz'>Submit</Link></Button>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default Login

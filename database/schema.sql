31ec9f9e4ba289d42a6fa07f88dbde085b157aad
Yvette <yvettetran@gmail.com>
2018-11-21 13:03:50 -0800

    created mysql schema

---
+++ database/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE doggo;
+USE doggo;
+
+    CREATE TABLE users (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR NOT NULL,
+        image VARCHAR NOT NULL,
+        score INT NOT NULL
+    );
+    
